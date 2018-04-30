eb2188b931c2828bedb94ccc6df8fc4c44b8ccc3
Maria  Morris <mariasemailbox@gmail.com>
2018-04-30 19:28:31 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,33 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+#DISPLAY BOARD
+def display_board(the_board)
+  puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{the_board[6]} | #{the_board[7]} | #{the_board[8]} "
 end
 
 # code your input_to_index and move method here!
+#MAIN PROGRAM
+#board = [" "," "," "," "," "," "," "," "," "]
+#display_board(board)
+#puts "Welcome to Tic Tac Toe!
+#Where would you like to go?"
+
+def input_to_index(input_from_user)
+  input = input_from_user.to_i
+  input - 1
+end
+
+def move(board, index, position='X')
+  board[index] = position
+end
+
+#board = [" "," "," "," "," "," "," "," "," "]
+#input_to_index = gets.strip
+#token = "x"
+#board[input_to_index.to_i] = token
+#input_to_index might = "0", the string 0
+#It's X's turn. How do we update board with an "x" at index 0 of the board?
+#How do we update data in an array?
+#display_board(board)
